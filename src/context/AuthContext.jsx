50e9200150e5649e150e484e575e174233e1c9b0
Aniket Botre <aniketbotre007@gmail.com>
2025-07-18 19:25:08 +0530

    feat: add global AuthContext with Firebase login/logout support

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,38 @@
+//! File: src/context/AuthContext.jsx 
+
+import { auth } from "@/lib/firebase";
+import { onAuthStateChanged, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
+import { createContext,useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+            setLoading(false);
+        });
+    }, [])
+
+    const loginWithGoogle = async () => {
+        const provider = new GoogleAuthProvider();
+        await signInWithPopup(auth, provider);
+    }
+
+    const logout = async () => {
+        await signOut(auth);
+    }
+
+    return (
+        <AuthContext.Provider value={{user, loginWithGoogle, logout, loading}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
