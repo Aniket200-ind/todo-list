127fbd9165946c8dc9db96f4946ecdd99464ece4
Aniket Botre <aniketbotre007@gmail.com>
2025-07-19 03:21:01 +0530

    refactor: Add conditional rendering for Sidebar and Theme toggler

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,16 +1,24 @@
 //! File: src/layout/AppLayout.jsx
 
-import ThemeToggle from '@/components/ThemeToggle';
+import ThemeToggle from "@/components/ThemeToggle";
+import Sidebar from "@/components/Sidebar";
+import { useLocation } from "react-router";
 
 export default function AppLayout({ children }) {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
   return (
-    <div className="min-h-screen flex flex-col bg-light-background dark:bg-dark-background transition-colors duration-300">
-      <header className="w-full flex justify-end p-4">
-        <ThemeToggle />
-      </header>
-      <main className="flex-1">
-        {children}
-      </main>
+    <div className="min-h-screen flex bg-light-background dark:bg-dark-background transition-colors duration-300">
+      {/* Sidebar only on non-login pages */}
+      {!isLoginPage && <Sidebar />}
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col">
+        <header className="w-full flex justify-end p-4">
+          {!isLoginPage && <ThemeToggle />}
+        </header>
+        <main className="flex-1 font-sora px-4 py-6">{children}</main>
+      </div>
     </div>
   );
 }
