a0097af5b91e9267f181435fc75c472761ec84fa
Aniket Botre <aniketbotre007@gmail.com>
2025-07-18 19:29:43 +0530

    feat: wrap app with AuthProvider for global authentication

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
 import { Toaster } from './components/ui/sonner'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <ThemeProvider>
     <App />
     <Toaster richColors position="bottom-right" />
     </ThemeProvider>
+    </AuthProvider>
   </StrictMode>,
 )
