02178b498643112efd5c680cf7f01ac80f2b31f3
Aniket Botre <aniketbotre007@gmail.com>
2025-08-06 19:43:32 +0530

    feat: Add loading state to ProtectedRoute component for better user experience

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,7 +4,16 @@ import useAuth from "@/hooks/useAuth";
 import { Navigate } from "react-router";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-light-accent"></div>
+      </div>
+    );
+  }
+
   if (!user) return <Navigate to="/login" replace />;
   return children;
 };
