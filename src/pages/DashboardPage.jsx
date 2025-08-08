bd7ac617a9f35d9e0ecf8fec8aa30fab6eea46a4
Aniket Botre <aniketbotre007@gmail.com>
2025-08-08 20:13:49 +0530

    refactor: Clean up DashboardPage component structure

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,6 @@
 //! File: src/pages.DashboardPage.jsx
 
+import ActivityChart from "@/components/dashboard/ActivityChart"
 import DashboardHeader from "@/components/dashboard/DashboardHeader"
 import Stats from "@/components/dashboard/Stats"
 
@@ -8,6 +9,7 @@ export const Dashboard = () => {
     <section className="space-y-10">
       <DashboardHeader />
       <Stats />
+      <ActivityChart />
     </section>
   )
 }
