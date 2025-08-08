99a129767bb90f1174d428517c60f8ad1118d5ab
Aniket Botre <aniketbotre007@gmail.com>
2025-08-08 21:59:13 +0530

    refactor: Add code formatting and remove unnecessary comments

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,8 +4,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-//? Firebase configuration
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -13,16 +11,11 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-//? Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-//? Intialize Firebase authentication and get a reference to the service 
 const auth = getAuth(app);
-
-//? Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
 export { app, auth, db };
